e654222c55ff4374ba9b5d43b751ea4ac6f4ffaa
DannisLi <zimengli@buaa.edu.cn>
2020-07-22 08:40:20 +0800

    a

--- selectivesearch/selectivesearch.py
+++ selectivesearch/selectivesearch.py
@@ -7,7 +7,7 @@ import skimage.color
 import skimage.transform
 import skimage.util
 import skimage.segmentation
-import numpy
+import numpy as np
 
 
 # "Selective Search for Object Recognition" by J.R.R. Uijlings et al.
@@ -27,8 +27,8 @@ def _generate_segments(im_orig, scale, sigma, min_size):
         min_size=min_size)
 
     # merge mask channel to the image as a 4th channel
-    im_orig = numpy.append(
-        im_orig, numpy.zeros(im_orig.shape[:2])[:, :, numpy.newaxis], axis=2)
+    im_orig = np.append(
+        im_orig, np.zeros(im_orig.shape[:2])[:, :, np.newaxis], axis=2)
     im_orig[:, :, 3] = im_mask
 
     return im_orig
@@ -83,16 +83,14 @@ def _calc_colour_hist(img):
     """
 
     BINS = 25
-    hist = numpy.array([])
+    hist = np.array([])
 
     for colour_channel in (0, 1, 2):
-
         # extracting one colour channel
         c = img[:, colour_channel]
-
         # calculate histogram for each colour and join to the result
-        hist = numpy.concatenate(
-            [hist] + [numpy.histogram(c, BINS, (0.0, 255.0))[0]])
+        hist = np.concatenate(
+            [hist] + [np.histogram(c, BINS, (0.0, 255.0))[0]])
 
     # L1 normalize
     hist = hist / len(img)
@@ -109,7 +107,7 @@ def _calc_texture_gradient(img):
 
         output will be [height(*)][width(*)]
     """
-    ret = numpy.zeros((img.shape[0], img.shape[1], img.shape[2]))
+    ret = np.zeros((img.shape[0], img.shape[1], img.shape[2]))
 
     for colour_channel in (0, 1, 2):
         ret[:, :, colour_channel] = skimage.feature.local_binary_pattern(
@@ -128,17 +126,15 @@ def _calc_texture_hist(img):
     """
     BINS = 10
 
-    hist = numpy.array([])
+    hist = np.array([])
 
     for colour_channel in (0, 1, 2):
-
         # mask by the colour channel
         fd = img[:, colour_channel]
-
         # calculate histogram for each orientation and concatenate them all
         # and join to the result
-        hist = numpy.concatenate(
-            [hist] + [numpy.histogram(fd, BINS, (0.0, 1.0))[0]])
+        hist = np.concatenate(
+            [hist] + [np.histogram(fd, BINS, (0.0, 1.0))[0]])
 
     # L1 Normalize
     hist = hist / len(img)
@@ -147,7 +143,6 @@ def _calc_texture_hist(img):
 
 
 def _extract_regions(img):
-
     R = {}
 
     # get hsv image
@@ -155,38 +150,33 @@ def _extract_regions(img):
 
     # pass 1: count pixel positions
     for y, i in enumerate(img):
-
         for x, (r, g, b, l) in enumerate(i):
-
             # initialize a new region
             if l not in R:
                 R[l] = {
-                    "min_x": 0xffff, "min_y": 0xffff,
-                    "max_x": 0, "max_y": 0, "labels": [l]}
-
-            # bounding box
-            if R[l]["min_x"] > x:
-                R[l]["min_x"] = x
-            if R[l]["min_y"] > y:
-                R[l]["min_y"] = y
-            if R[l]["max_x"] < x:
-                R[l]["max_x"] = x
-            if R[l]["max_y"] < y:
-                R[l]["max_y"] = y
-
+                    "min_x": x, "min_y": y,
+                    "max_x": x, "max_y": y, "labels": set([l])}
+            else:
+                # bounding box
+                if R[l]["min_x"] > x:
+                    R[l]["min_x"] = x
+                if R[l]["min_y"] > y:
+                    R[l]["min_y"] = y
+                if R[l]["max_x"] < x:
+                    R[l]["max_x"] = x
+                if R[l]["max_y"] < y:
+                    R[l]["max_y"] = y
     # pass 2: calculate texture gradient
     tex_grad = _calc_texture_gradient(img)
-
     # pass 3: calculate colour histogram of each region
-    for k, v in list(R.items()):
-
+    for k in R.keys():
         # colour histogram
-        masked_pixels = hsv[:, :, :][img[:, :, 3] == k]
-        R[k]["size"] = len(masked_pixels / 4)
+        masked_pixels = hsv[img[:, :, 3] == k]
+        # R[k]["size"] = len(masked_pixels / 4)
+        R[k]["size"] = len(masked_pixels)
         R[k]["hist_c"] = _calc_colour_hist(masked_pixels)
-
         # texture histogram
-        R[k]["hist_t"] = _calc_texture_hist(tex_grad[:, :][img[:, :, 3] == k])
+        R[k]["hist_t"] = _calc_texture_hist(tex_grad[img[:, :, 3] == k])
 
     return R
 
@@ -194,14 +184,14 @@ def _extract_regions(img):
 def _extract_neighbours(regions):
 
     def intersect(a, b):
-        if (a["min_x"] < b["min_x"] < a["max_x"]
-                and a["min_y"] < b["min_y"] < a["max_y"]) or (
-            a["min_x"] < b["max_x"] < a["max_x"]
-                and a["min_y"] < b["max_y"] < a["max_y"]) or (
-            a["min_x"] < b["min_x"] < a["max_x"]
-                and a["min_y"] < b["max_y"] < a["max_y"]) or (
-            a["min_x"] < b["max_x"] < a["max_x"]
-                and a["min_y"] < b["min_y"] < a["max_y"]):
+        if (a["min_x"] <= b["min_x"] <= a["max_x"]
+                and a["min_y"] <= b["min_y"] <= a["max_y"]) or (
+            a["min_x"] <= b["max_x"] <= a["max_x"]
+                and a["min_y"] <= b["max_y"] <= a["max_y"]) or (
+            a["min_x"] <= b["min_x"] <= a["max_x"]
+                and a["min_y"] <= b["max_y"] <= a["max_y"]) or (
+            a["min_x"] <= b["max_x"] <= a["max_x"]
+                and a["min_y"] <= b["min_y"] <= a["max_y"]):
             return True
         return False
 
@@ -227,7 +217,7 @@ def _merge_regions(r1, r2):
             r1["hist_c"] * r1["size"] + r2["hist_c"] * r2["size"]) / new_size,
         "hist_t": (
             r1["hist_t"] * r1["size"] + r2["hist_t"] * r2["size"]) / new_size,
-        "labels": r1["labels"] + r2["labels"]
+        "labels": r1["labels"].union(r2["labels"])
     }
     return rt
 
@@ -283,29 +273,25 @@ def selective_search(
 
     # hierarchal search
     while S != {}:
-
         # get highest similarity
         i, j = sorted(S.items(), key=lambda i: i[1])[-1][0]
-
         # merge corresponding regions
         t = max(R.keys()) + 1.0
         R[t] = _merge_regions(R[i], R[j])
-
         # mark similarities for regions to be removed
         key_to_delete = []
         for k, v in list(S.items()):
             if (i in k) or (j in k):
                 key_to_delete.append(k)
-
         # remove old similarities of related regions
         for k in key_to_delete:
             del S[k]
-
         # calculate similarity set with the new region
         for k in [a for a in key_to_delete if a != (i, j)]:
             n = k[1] if k[0] in (i, j) else k[0]
             S[(t, n)] = _calc_sim(R[t], R[n], imsize)
 
+    '''
     regions = []
     for k, r in list(R.items()):
         regions.append({
@@ -315,5 +301,66 @@ def selective_search(
             'size': r['size'],
             'labels': r['labels']
         })
+    '''
+    regions = set()
+    for k, r in list(R.items()):
+        regions.add((r['min_x'], r['min_y'], r['max_x'] - r['min_x'], r['max_y'] - r['min_y']))
+
 
     return img, regions
+
+
+
+if __name__ == "__main__":
+    import skimage.data
+    from skimage.io import imread
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+    
+
+    # loading astronaut image
+    # img = skimage.data.astronaut()
+    img = imread("D:\\works\\Xray\\train\\domain1\\100010.jpg")
+
+    # perform selective search
+    img_lbl, regions = selective_search(
+        img, scale=400, sigma=0.9, min_size=20)
+    
+    imsize = img.shape[0] * img.shape[1]
+    
+    cnt = 0
+    for r in regions:
+        if 0.05 <= r[2]*r[3] / imsize <= 0.9:
+            print (r)
+            cnt += 1
+
+    print (cnt)
+    
+
+    '''
+    candidates = set()
+    for r in regions:
+        # excluding same rectangle (with different segments)
+        # print (r["rect"], r["size"])
+        if r['rect'] in candidates:
+            continue
+        # excluding regions smaller than 2000 pixels
+        if r['size'] < 4000:
+            continue
+        # distorted rects
+        x, y, w, h = r['rect']
+        if w / h > 1.2 or h / w > 1.2:
+            continue
+        candidates.add(r['rect'])
+    '''
+    # draw rectangles on the original image
+    fig, ax = plt.subplots(ncols=1, nrows=1)
+    ax.imshow(img)
+    for x, y, w, h in regions:
+        if 0.01 <= w*h / imsize <= 0.9:
+            rect = mpatches.Rectangle(
+                (x, y), w, h, fill=False, edgecolor='red', linewidth=1)
+            ax.add_patch(rect)
+
+    plt.show()
+    
