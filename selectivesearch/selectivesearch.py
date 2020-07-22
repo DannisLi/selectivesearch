f7ceaf027f72d2802ef181e1e4cc0b8f88718561
DannisLi <zimengli@buaa.edu.cn>
2020-07-22 08:45:53 +0800

    a

--- selectivesearch/selectivesearch.py
+++ selectivesearch/selectivesearch.py
@@ -310,7 +310,7 @@ def selective_search(
     return img, regions
 
 
-
+"""
 if __name__ == "__main__":
     import skimage.data
     from skimage.io import imread
@@ -320,7 +320,7 @@ if __name__ == "__main__":
 
     # loading astronaut image
     # img = skimage.data.astronaut()
-    img = imread("D:\\works\\Xray\\train\\domain1\\100010.jpg")
+    img = imread("D:\\works\\Xray\\train\\domain1\\100013.jpg")
 
     # perform selective search
     img_lbl, regions = selective_search(
@@ -330,37 +330,20 @@ if __name__ == "__main__":
     
     cnt = 0
     for r in regions:
-        if 0.05 <= r[2]*r[3] / imsize <= 0.9:
-            print (r)
+        if 0.02 <= r[2]*r[3] / imsize <= 0.9:
+            # print (r)
             cnt += 1
 
     print (cnt)
     
-
-    '''
-    candidates = set()
-    for r in regions:
-        # excluding same rectangle (with different segments)
-        # print (r["rect"], r["size"])
-        if r['rect'] in candidates:
-            continue
-        # excluding regions smaller than 2000 pixels
-        if r['size'] < 4000:
-            continue
-        # distorted rects
-        x, y, w, h = r['rect']
-        if w / h > 1.2 or h / w > 1.2:
-            continue
-        candidates.add(r['rect'])
-    '''
     # draw rectangles on the original image
     fig, ax = plt.subplots(ncols=1, nrows=1)
     ax.imshow(img)
     for x, y, w, h in regions:
-        if 0.01 <= w*h / imsize <= 0.9:
+        if 0.02 <= w*h / imsize <= 0.9:
             rect = mpatches.Rectangle(
                 (x, y), w, h, fill=False, edgecolor='red', linewidth=1)
             ax.add_patch(rect)
 
     plt.show()
-    
+"""
